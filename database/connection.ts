cb4321424c531c412a786b274e8e961ada6e7d6b
jonchronos <joan.g@chronosnetwork.io>
2022-10-24 18:18:32 -0300

    [DEL] dotenv imports

--- database/connection.ts
+++ database/connection.ts
@@ -1,6 +1,4 @@
 import { Sequelize } from "sequelize";
-import dotenv from 'dotenv'
-dotenv.config()
 
 const database = new Sequelize('evently', 'root', process.env.DB_PASS, {
     host: 'localhost',
