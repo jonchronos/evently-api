bb8026f4e32e3f8d6fa061d780f7059be7a415e9
jonchronos <joan.g@chronosnetwork.io>
2022-10-24 07:49:14 -0300

    .

--- controllers/users.ts
+++ controllers/users.ts
@@ -1,10 +1,16 @@
 import { Response, Request } from 'express'
+import User from '../models/user'
 
 
 // Get all users
-export const getUsers = (_: Request, res: Response) => {
+export const getUsers = async (_: Request, res: Response) => {
+    const users = await User.findAll({
+        where: {
+            status: true
+        }
+    })
     res.json({
-        msg: 'hola'
+        users
     })
 }
 //Get a user by ID
@@ -15,8 +21,18 @@ export const getUser = (_: Request, res: Response) => {
     })
 }
 //Create new user
-export const createUser = (_: Request, res: Response) => {
-    res.send('Hola desde typescript')
+export const createUser = async (req: Request, res: Response) => {
+    const { full_name, email, password } = req.body
+
+    const user = await User.create({
+        full_name,
+        email,
+        password
+    })
+
+    res.json({
+        user
+    })
 }
 // Update user by ID
 export const updateUser = (_: Request, res: Response) => {
