dca28279788bd5446c1096b3d62590bdab52b4ea
jonchronos <joan.g@chronosnetwork.io>
2022-10-24 18:15:54 -0300

    [ADD] Auth types

--- types.d.ts
+++ types.d.ts
@@ -1,3 +1,17 @@
-export interface ApiPath {
-    users: String
+declare namespace NodeJS {
+    export interface ProcessEnv {
+        HOST: string;
+        DB_PASS: string;
+        PRIVATE_KEY: string;
+    }
+
+}
+export type Auth = {
+    pk: number | null
+    id: string
+    full_name: string | null
+    email: string | null
+    password: string
+    status: boolean | null
+    google: boolean | null
 }
