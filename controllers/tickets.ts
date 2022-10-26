3c2777a5ce8d36249480291018fec9a6b5b3f37c
jonchronos <joan.g@chronosnetwork.io>
2022-10-26 11:50:11 -0300

    [ADD] tickets controller

--- controllers/tickets.ts
+++ controllers/tickets.ts
@@ -1,15 +1,25 @@
 import { Response, Request } from 'express'
-import Tickets from '../models/ticket'
+import Ticket from '../models/ticket'
 
 
-export const getTickets = (_: Request, res: Response) => {
-    const tickets = Tickets.findAll({
+export const getTickets = async (_: Request, res: Response) => {
+    const tickets = await Ticket.findAll({
         where: {
             status: true
-        }
+        },
+        include: ['event']
     })
 
     res.json({
         tickets
     })
 }
+
+export const createTicket = async (req: Request, res: Response) => {
+    const body = req.body
+
+    const ticket = await Ticket.create(body)
+
+    res.json(ticket)
+}
+
