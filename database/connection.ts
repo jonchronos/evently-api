976cbdb71834bbee8eb1158c91dea19b319d865e
jonchronos <joan.g@chronosnetwork.io>
2022-10-26 19:59:48 -0300

    [ADD] connect to railway

--- database/connection.ts
+++ database/connection.ts
@@ -1,8 +1,9 @@
 import { Sequelize } from "sequelize";
 
-const database = new Sequelize('evently', 'root', process.env.DB_PASS, {
-    host: 'localhost',
-    dialect: 'mysql'
-})
+const database = new Sequelize('mysql://root:19yLCQqYNRF2vek8sPTR@containers-us-west-102.railway.app:6832/railway')
+// const database = new Sequelize('evently', 'root', process.env.DB_PASS, {
+//     host: 'localhost',
+//     dialect: 'mysql'
+// })
 
 export default database
--- dist/database/connection.js
+++ dist/database/connection.js
@@ -1,8 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const sequelize_1 = require("sequelize");
-const database = new sequelize_1.Sequelize('evently', 'root', process.env.DB_PASS, {
-    host: 'localhost',
-    dialect: 'mysql'
-});
+const database = new sequelize_1.Sequelize('mysql://root:19yLCQqYNRF2vek8sPTR@containers-us-west-102.railway.app:6832/railway');
+// const database = new Sequelize('evently', 'root', process.env.DB_PASS, {
+//     host: 'localhost',
+//     dialect: 'mysql'
+// })
 exports.default = database;
