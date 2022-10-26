2756b6ec814271025e2ba8bb1fb869412cecafbe
jonchronos <joan.g@chronosnetwork.io>
2022-10-26 11:50:33 -0300

    [ADD] getUserTickets

--- controllers/users.ts
+++ controllers/users.ts
@@ -1,6 +1,9 @@
 import { Response, Request } from 'express'
 import bcryptjs from 'bcryptjs'
 import User from '../models/user'
+import Booking from '../models/booking'
+// import { Auth } from '../types'
+
 
 
 // Get all users
@@ -8,11 +11,12 @@ export const getUsers = async (_: Request, res: Response) => {
     const users = await User.findAll({
         where: {
             status: true
-        }
+        },
+        include: ['tickets']
     })
-    res.json({
+    res.json(
         users
-    })
+    )
 }
 //Get a user by ID
 export const getUser = (_: Request, res: Response) => {
@@ -20,6 +24,17 @@ export const getUser = (_: Request, res: Response) => {
         msg: 'hola'
     })
 }
+// Get user's tickets
+export const getUserTickets = async (req: Request, res: Response) => {
+    const { id } = req.params
+
+    const tickets = await Booking.findAll({
+        where: { user_id: id }
+    })
+
+    res.json(tickets)
+}
+
 //Create new user
 export const createUser = async (req: Request, res: Response) => {
     const { full_name, email, password } = req.body
@@ -44,5 +59,7 @@ export const updateUser = (_: Request, res: Response) => {
 export const deleteUser = (_: Request, res: Response) => {
     res.send('Hola desde typescript')
 }
+
+
 
 
