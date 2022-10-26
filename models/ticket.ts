04e5e916fad913c2992b38f53e94711d5a7784d5
jonchronos <joan.g@chronosnetwork.io>
2022-10-26 11:51:57 -0300

    [ADD] default values

--- models/ticket.ts
+++ models/ticket.ts
@@ -1,8 +1,9 @@
 import { DataTypes } from 'sequelize'
 import db from '../database/connection'
+import Event from '../models/events'
 
 
-const Ticket = db.define('ticket', {
+const Ticket = db.define('Ticket', {
     pk: {
         type: DataTypes.INTEGER,
         primaryKey: true,
@@ -13,29 +14,34 @@ const Ticket = db.define('ticket', {
         defaultValue: DataTypes.UUIDV4
     },
     name: {
-        type: DataTypes.STRING
+        type: DataTypes.STRING,
+        defaultValue: 'Metalica'
     },
-    date: {
-        type: DataTypes.DATEONLY,
-        allowNull: false
+    area: {
+        type: DataTypes.STRING,
+        defaultValue: 'General'
     },
-    time: {
-        type: DataTypes.TIME,
-        allowNull: false
+    price: {
+        type: DataTypes.INTEGER,
+        defaultValue: 4000
     },
-    location: {
-        type: DataTypes.STRING
-    },
-    address: {
-        type: DataTypes.STRING
+    availables: {
+        type: DataTypes.INTEGER,
+        defaultValue: 42
     },
     status: {
-        type: DataTypes.BOOLEAN
+        type: DataTypes.BOOLEAN,
+        defaultValue: true
     },
 },
     {
         timestamps: false
     }
 )
+
+//Associations
+//User has many Bookings
+Ticket.hasOne(Event, { foreignKey: 'event_id', as: 'event' });
+Event.belongsTo(Ticket, { foreignKey: 'event_id', as: 'ticket' });
 
 export default Ticket
